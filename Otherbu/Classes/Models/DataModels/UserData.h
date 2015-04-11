b6bf1d8964841cc5123e9bf15baa058bdb9b420a
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-04-12 05:04:16 +0900

    ディレクトリ構造の変更

---
+++ Otherbu/Classes/Models/DataModels/UserData.h
@@ -0,0 +1,13 @@
+//
+//  UserData.h
+//  Otherbu
+//
+//  Created by fujimisakari on 2015/04/12.
+//  Copyright (c) 2015年 fujimisakari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserData : NSObject
+
+@end
---
+++ Otherbu/Classes/Models/DataModels/UserData.m
@@ -0,0 +1,13 @@
+//
+//  UserData.m
+//  Otherbu
+//
+//  Created by fujimisakari on 2015/04/12.
+//  Copyright (c) 2015年 fujimisakari. All rights reserved.
+//
+
+#import "UserData.h"
+
+@implementation UserData
+
+@end
