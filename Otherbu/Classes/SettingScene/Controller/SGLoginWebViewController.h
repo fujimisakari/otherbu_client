afa97c706567610c09175924b46cc8eb2f6925e4
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-07-21 03:53:08 +0900

    不要コードを削除

--- Otherbu/Classes/SettingScene/Controller/SGLoginWebViewController.h
+++ Otherbu/Classes/SettingScene/Controller/SGLoginWebViewController.h
@@ -6,16 +6,10 @@
 //  Copyright (c) 2015 fujimisakari. All rights reserved.
 //
 
-// #import "ModalInterface.h"
-
-// @class NavigationBar, CustomWebView, BookmarkData;
 @class LoginWebView;
 
 @interface SGLoginWebViewController : UIViewController<UIWebViewDelegate>
-// @interface SGWebViewController : UIViewController<UIScrollViewDelegate, ModalInterface>
 
-// @property(weak, nonatomic) IBOutlet NavigationBar *navigationBar;
 @property(weak, nonatomic) IBOutlet LoginWebView *webView;
-// @property(nonatomic) BookmarkData *bookmark;
 
 @end
--- Otherbu/Classes/SettingScene/Controller/SGLoginWebViewController.m
+++ Otherbu/Classes/SettingScene/Controller/SGLoginWebViewController.m
@@ -7,13 +7,9 @@
 //
 
 #import "SGLoginWebViewController.h"
-// #import "ModalBKViewController.h"
-// #import "NavigationBar.h"
-// #import "BookmarkData.h"
 #import "LoginWebView.h"
 
 @interface SGLoginWebViewController () {
-    // id<DataInterface> currentWebPage;
 }
 
 @end
@@ -29,20 +25,12 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-
-    // NavigationBar設定
-    // [_navigationBar setup];
-    // [_navigationBar setButtonInWebViewScene];
-    // _navigationBar.topItem.leftBarButtonItem.target = self;
-    // _navigationBar.topItem.leftBarButtonItem.action = @selector(_addBookmark:);
-    // _navigationBar.topItem.rightBarButtonItem.target = self;
-    // _navigationBar.topItem.rightBarButtonItem.action = @selector(_closeWebView:);
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     // 画面が表示され終ったらWebPageの読み込み
     [super viewDidAppear:animated];
-    NSURL *url = [NSURL URLWithString:@"http://otherbu.com"];
+    NSURL *url = [NSURL URLWithString:@"http://dev.otherbu.com/login/client/"];
     NSURLRequest *urlReq = [NSURLRequest requestWithURL:url];
     [_webView loadRequest:urlReq];
 }
@@ -50,36 +38,8 @@
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
 
-    // [_navigationBar deleteButtonInWebViewScene];
-
     // 画面を閉じるときにステータスバーのインジケータ確実にOFFにする
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 }
-
-//--------------------------------------------------------------//
-#pragma mark-- Navi Button Event--
-//--------------------------------------------------------------//
-
-- (void)_closeWebView:(UIButton *)sender {
-    // WebViewを閉じる
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-// - (void)_addBookmark:(UIButton *)sender {
-//     // 現在表示しているページの名前とURLを入れてBookmark追加画面へ遷移する
-//     NSString *name = [_webView stringByEvaluatingJavaScriptFromString:@"document.title"];
-//     NSString *url = [_webView stringByEvaluatingJavaScriptFromString:@"document.URL"];
-
-//     currentWebPage = [BookmarkData alloc];
-//     [currentWebPage iSetName:name];
-//     [currentWebPage iSetUrl:url];
-
-//     ModalBKViewController *modalBKViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"ModalBKViewController"];
-//     modalBKViewController.delegate = self;
-//     modalBKViewController.editItem = currentWebPage;
-//     [self presentViewController:modalBKViewController animated:YES completion:nil];
-// }
-
-// - (void)returnActionOfModal:(NSInteger)menuId{};
 
 @end
