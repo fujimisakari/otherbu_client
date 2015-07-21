5882e660b812707bbf57113582e6c59653cabb13
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-07-22 01:40:14 +0900

    webviewのload処理を更新

--- Otherbu/Classes/SettingScene/View/LoginWebView.m
+++ Otherbu/Classes/SettingScene/View/LoginWebView.m
@@ -29,6 +29,39 @@
 #pragma mark -- UIWebViewDelegate --
 //--------------------------------------------------------------//
 
+- (BOOL)webView:(UIWebView *)webView
+shouldStartLoadWithRequest:(NSURLRequest *)request
+            navigationType:(UIWebViewNavigationType)navigationType {
+    // Webページのロード（表示）の開始前
+    // リンクがクリックされたとき
+    if (navigationType == UIWebViewNavigationTypeLinkClicked ||
+        navigationType == UIWebViewNavigationTypeOther) {
+
+        NSString *url = [[request URL] absoluteString];
+        if ([self _isRequestCheck:url]) {
+            NSString *urlString = [NSString stringWithFormat:@"%@client/", url];
+            NSURL *loadURL = [NSURL URLWithString:urlString];
+            NSURLRequest *urlReq = [NSURLRequest requestWithURL:loadURL];
+            [self loadRequest:urlReq];
+            return NO;
+        }
+    }
+    return YES;
+}
+
+- (BOOL)_isRequestCheck:(NSString *)url {
+    NSString *facebookUrlString = @"/oauth/facebook/";
+    NSString *twitterUrlString = @"/oauth/twitter/";
+    if ([url rangeOfString:@"client"].location != NSNotFound) {
+        return NO;
+    } else if ([url rangeOfString:facebookUrlString].location != NSNotFound) {
+        return YES;
+    } else if ([url rangeOfString:twitterUrlString].location != NSNotFound) {
+        return YES;
+    }
+    return NO;
+}
+
 - (void)webViewDidStartLoad:(UIWebView *)webview {
     // 読み込みが開始した直後に呼ばれる
     [self _updateStatusbar];
